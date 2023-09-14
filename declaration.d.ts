d17152be3e3a11b9b1994bce6d5aa8c586c40d5e
luisfelipe2104 <gomesluisfelipe05@gmail.com>
2023-09-14 05:30:02 -0300

    linking firebase to project

--- declaration.d.ts
+++ declaration.d.ts
@@ -1,2 +1,11 @@
 declare module '*.jpg';
 declare module '*.png';
+declare module '@env' {
+    export const apiKey: string;
+    export const authDomain: string;
+    export const projectId: string;
+    export const storageBucket: string;
+    export const messagingSenderId: string;
+    export const appId: string;
+    export const measurementId: string;
+}
